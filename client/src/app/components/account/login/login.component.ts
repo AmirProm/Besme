a85d79ff4de978d20124f5f35dd5b07032c7f846
AmirProm <promgrameramir@gmail.com>
2025-05-12 00:50:46 +0330

    set component login

--- client/src/app/components/account/login/login.component.ts
+++ client/src/app/components/account/login/login.component.ts
@@ -1,48 +1,49 @@
-// import { Component, inject } from '@angular/core';
-// import { AccountService } from '../../../services/account.service';
-// import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-// import { Login } from '../../../models/login.model';
-// import { RouterLink } from '@angular/router';
-// import { MatButtonModule } from '@angular/material/button';
-// import { MatFormFieldModule } from '@angular/material/form-field';
-// import { MatInputModule } from '@angular/material/input';
-// import { MatCardModule } from '@angular/material/card';
+import { Component, inject } from '@angular/core';
+import { AccountService } from '../../../services/account‍‍.service';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AppUser } from '../../../../models/app-user.model';
+import { Login } from '../../../../models/login.model';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
-// @Component({
-//   selector: 'app-login',
-//   standalone: true,
-//   imports: [
-//     RouterLink,
-//     FormsModule, ReactiveFormsModule,
-//     MatCardModule,
-//     MatButtonModule, MatFormFieldModule, MatInputModule,
-//   ],
-//   templateUrl: './login.component.html',
-//   styleUrl: './login.component.scss'
-// })
-// export class LoginComponent {
-//   accountService = inject(AccountService);
-//   fB = inject(FormBuilder);
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    RouterLink,
+    FormsModule, ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule, MatFormFieldModule, MatInputModule,
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  accountService = inject(AccountService);
+  fB = inject(FormBuilder);
 
-//   loginFg = this.fB.group({
-//     emailCtrl: ['', [Validators.required, Validators.email]],
-//     passwordCtrl: ['']
-//   })
+  loginFg = this.fB.group({
+    emailCtrl: ['', [Validators.required, Validators.email]],
+    passwordCtrl: ['']
+  })
 
-//   get EmailCtrl(): FormControl {
-//     return this.loginFg.get('emailCtrl') as FormControl;
-//   }
+  get EmailCtrl(): FormControl {
+    return this.loginFg.get('emailCtrl') as FormControl;
+  }
 
-//   get PasswordCtrl(): FormControl {
-//     return this.loginFg.get('passwordCtrl') as FormControl;
-//   }
+  get PasswordCtrl(): FormControl {
+    return this.loginFg.get('passwordCtrl') as FormControl;
+  }
 
-//   login(): void {
-//     let userInput: Login = {
-//       email: this.EmailCtrl.value,
-//       password: this.PasswordCtrl.value
-//     }
+  login(): void {
+    let userInput: Login = {
+      email: this.EmailCtrl.value,
+      password: this.PasswordCtrl.value
+    }
 
-//     this.accountService.login(userInput).subscribe();
-//   }
-// }
+    this.accountService.login(userInput).subscribe();
+  }
+}
