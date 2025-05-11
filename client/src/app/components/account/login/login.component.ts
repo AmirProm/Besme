b9b1261fa9ba117b3b33bb6e4a96310bcc0582c5
AmirProm <promgrameramir@gmail.com>
2025-05-12 00:41:46 +0330

    create model && DTOs

--- client/src/app/components/account/login/login.component.ts
+++ client/src/app/components/account/login/login.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+// import { Component, inject } from '@angular/core';
+// import { AccountService } from '../../../services/account.service';
+// import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+// import { Login } from '../../../models/login.model';
+// import { RouterLink } from '@angular/router';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatFormFieldModule } from '@angular/material/form-field';
+// import { MatInputModule } from '@angular/material/input';
+// import { MatCardModule } from '@angular/material/card';
 
-@Component({
-  selector: 'app-login',
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
-})
-export class LoginComponent {
+// @Component({
+//   selector: 'app-login',
+//   standalone: true,
+//   imports: [
+//     RouterLink,
+//     FormsModule, ReactiveFormsModule,
+//     MatCardModule,
+//     MatButtonModule, MatFormFieldModule, MatInputModule,
+//   ],
+//   templateUrl: './login.component.html',
+//   styleUrl: './login.component.scss'
+// })
+// export class LoginComponent {
+//   accountService = inject(AccountService);
+//   fB = inject(FormBuilder);
 
-}
+//   loginFg = this.fB.group({
+//     emailCtrl: ['', [Validators.required, Validators.email]],
+//     passwordCtrl: ['']
+//   })
+
+//   get EmailCtrl(): FormControl {
+//     return this.loginFg.get('emailCtrl') as FormControl;
+//   }
+
+//   get PasswordCtrl(): FormControl {
+//     return this.loginFg.get('passwordCtrl') as FormControl;
+//   }
+
+//   login(): void {
+//     let userInput: Login = {
+//       email: this.EmailCtrl.value,
+//       password: this.PasswordCtrl.value
+//     }
+
+//     this.accountService.login(userInput).subscribe();
+//   }
+// }
---
+++ client/src/app/services/account‍‍.service.ts
@@ -0,0 +1,72 @@
+// import { HttpClient } from '@angular/common/http';
+// import { inject, Injectable, PLATFORM_ID } from '@angular/core';
+// import { LoggedIn } from '../models/logged-in.model';
+// import { AppUser } from '../models/app-user.model';
+// import { map, Observable } from 'rxjs';
+// import { Login } from '../models/login.model';
+// import { Member } from '../models/member.model';
+// import { isPlatformBrowser } from '@angular/common';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AccountService {
+//   http = inject(HttpClient);
+//   private readonly _baseApiUrl: string = 'http://localhost:5000/api/';
+//   platformId = inject(PLATFORM_ID);
+
+//   register(user: AppUser): Observable<LoggedIn> {
+//     return this.http.post<LoggedIn>(this._baseApiUrl + 'account/register', user);
+//   }
+
+//   login(userInput: Login): Observable<LoggedIn> {
+//     return this.http.post<LoggedIn>(
+//       this._baseApiUrl + 'account/login', userInput).pipe(
+//         map(userResponse => {
+//           this.setCurrentUser(userResponse);
+
+//           return userResponse;
+//         })
+//       )
+//   }
+  
+
+//   getAllMember(): Observable<Member[]> {
+//     return this.http.get<Member[]>(this._baseApiUrl + 'member');
+//   }
+
+//   setCurrentUser(loggedInUser: LoggedIn): void {
+//     if (isPlatformBrowser(this.platformId)) {
+//       localStorage.setItem('loggedInUser', JSON.stringify(loggedInUser));
+//     }
+//   }
+// }
+
+
+
+
+
+
+// /* set current user
+//   platformId = inject(PLATFORM_ID);
+// login(userInput: Login): Observable<LoggedIn | null> {
+//     return this.http.post<LoggedIn>(this._baseApiUrl + 'account/login', userInput).pipe(
+//       map(userResponse => {
+//         if (userResponse) {
+//           this.setCurrentUser(userResponse)
+          
+//           return userResponse;
+//           }
+          
+//         return null;
+//       })
+//     )
+          
+//   }
+//     setCurrentUser(loggedInUser: LoggedIn): void {
+//     if (isPlatformBrowser(this.platformId)) {
+//     console.log(this.platformId);
+//     localStorage.setItem('loggedInUser', JSON.stringify(loggedInUser))
+//   }
+// }
+// */
