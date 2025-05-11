798b902414c3a4517acec3589544a713a4d976e8
AmirProm <promgrameramir@gmail.com>
2025-05-12 00:31:54 +0330

    app.components END

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterOutlet, RouterModule, RouterLink } from '@angular/router';
+// import { AccountService } from './services/account.service';
+// import { AppUser } from './models/app-user.model';
+// import { Login } from './models/login.model';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet, RouterModule, RouterLink,
+    FormsModule, ReactiveFormsModule,
+    MatButtonModule,
+    NavbarComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'client';
+  // accountService = inject(AccountService);
 }
